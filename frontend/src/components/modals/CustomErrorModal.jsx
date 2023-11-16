8e64cde307dd49cc63da75be6ff04633b82a09a9
Phot Koseekrainiramon <z5387411@ad.unsw.edu.au>
2023-11-16 17:52:57 +1100

    update: using toast instead of modal for success/error popup

--- frontend/src/components/modals/CustomErrorModal.jsx
+++ frontend/src/components/modals/CustomErrorModal.jsx
@@ -1,39 +1,24 @@
 import React from 'react';
 
-import Modal from '@mui/material/Modal';
-import Box from '@mui/material/Box';
-import { IconButton, Typography } from '@mui/material';
-import CloseIcon from '@mui/icons-material/Close';
-import { style } from './ReviewModal';
+import { Alert, Snackbar } from '@mui/material';
 
 // a cutom error modal showing custom error message
 const CustomErrorModal = (props) => {
   return (
     <>
-      <Modal
+      <Snackbar
         open={props.openError}
+        autoHideDuration={5000}
         onClose={() => props.setOpenError(false)}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
       >
-        <Box sx={style}>
-          <div className="text-lg font-bold mb-5">
-            {props.error ? 'Error' : 'Success'}
-          </div>
-
-          <Box className="absolute top-2 right-2">
-            <IconButton onClick={() => props.setOpenError(false)}>
-              <CloseIcon />
-            </IconButton>
-          </Box>
-
-          <div className="flex flex-col flex-wrap gap-2 w-full">
-            <Typography variant="body2" color="text.secondary">
-              {props.error}
-            </Typography>
-          </div>
-        </Box>
-      </Modal>
+        <Alert
+          onClose={() => props.setOpenError(false)}
+          severity={props.error ? 'error' : 'success'}
+          sx={{ width: '100%' }}
+        >
+          {props.error ? props.error : 'success'}
+        </Alert>
+      </Snackbar>
     </>
   );
 }
